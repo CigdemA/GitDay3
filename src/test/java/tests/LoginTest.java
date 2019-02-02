93528f1c176979ffb59fcaf4dafd2d975b0145af
CigdemA <46769594+CigdemA@users.noreply.github.com>
2019-02-01 22:19:53 -0500

    Update LoginTest.java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -29,14 +29,16 @@ public class LoginTest {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
-        Assert.assertTrue(driver.getTitle().equals("Web Orders"));
+        String title = driver.getTitle();
+        Assert.assertTrue(title.equals("Web Orders"));
     }
 
     @Test
     public void logoutTest() {
         loginTest1();
         driver.findElement(By.id("ctl00_logout")).click();
-        Assert.assertTrue(driver.getTitle().equals("Web Orders Login"));
+        String title = driver.getTitle();
+        Assert.assertTrue(title.equals("Web Orders Login"));
     }
 
     @AfterMethod
